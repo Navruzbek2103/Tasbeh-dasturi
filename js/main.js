ce729ece1145a4ccc37e2423698596c82066bbd4
Navruzbek2103 <navrozbek2306@gmail.com>
2022-07-26 10:39:13 +0500

    Editted theme section

--- js/main.js
+++ js/main.js
@@ -13,17 +13,20 @@ elRetryBtn.addEventListener("click", function(){
   var clear = 0;
   resultValue.value = clear
 })
-
+var compute = 0;
 elColorBtn.addEventListener("click", function(){
-  var compute = 0;
 
   compute++;
-  if(compute == 1){
-    resultValue.style.backgroundColor = "red"
-  }
-  else if(compute != 1){
+  if(compute % 2 == 0){
     resultValue.style.backgroundColor = "white"
+    resultValue.style.boxShadow = "0px 0px 3px 2px white"
+  }
+  else{
+    resultValue.style.backgroundColor = "cyan"
+    resultValue.style.boxShadow = "0px 0px 5px 2px cyan"
 
   }
+
+
   // alert(compute)
 })
